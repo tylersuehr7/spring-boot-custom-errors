fb65be03aa3099ff6d75b9ae17bf8c709e690c0d
Tyler Suehr <tylersuehr@gmail.com>
2021-06-23 21:20:29 -0400

    Displays custom error response for all exceptions

--- src/main/kotlin/com/tylersuehr/poc/customerrors/errors/ErrorController.kt
+++ src/main/kotlin/com/tylersuehr/poc/customerrors/errors/ErrorController.kt
@@ -1,9 +1,17 @@
 package com.tylersuehr.poc.customerrors.errors
 
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.handler.ResponseStatusExceptionHandler
+import org.springframework.web.context.request.ServletWebRequest
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import javax.servlet.http.HttpServletRequest
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.hasAnnotation
+
 
 /**
  * A custom error handler for all exceptions.
@@ -12,4 +20,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 @ControllerAdvice
 internal class ErrorController : ResponseEntityExceptionHandler() {
+    override fun handleExceptionInternal(
+        ex: Exception,
+        body: Any?,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        // Parse the custom error from exception
+        var customCode: CustomErrorCode = CustomErrorCode.NONE
+        if (ex::class.hasAnnotation<CustomResponseCode>()) {
+            customCode = ex::class.findAnnotation<CustomResponseCode>()!!.code
+        }
+
+        // Create the data transfer object for error
+        val dto = ErrorDto(
+            path = (request as ServletWebRequest).request.requestURI,
+            code = status.value(),
+            error = status.reasonPhrase,
+            message = if (ex.localizedMessage == null || ex.localizedMessage.isEmpty()) "Something went wrong!" else ex.localizedMessage,
+            customCode = customCode.code
+        )
+
+        return ResponseEntity(dto, status)
+    }
 }
